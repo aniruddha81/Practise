c91805f5ebeed8de16d9c3f0e412e598ab2ed479
aniruddha81 <ar.roy564@gmail.com>
2024-11-28 03:59:58 +0600

    2nd time

--- test.js
+++ test.js
@@ -1,2 +1,7 @@
-console.log("hitesh");
+const accountId = 144553
+let accountEmail = "ar.roy564@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
 
